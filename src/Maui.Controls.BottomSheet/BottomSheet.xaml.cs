cd0f87376080d572d71e9218e11a21eccd473e6d
Naweed Akram <naweed@xgeno.com>
2022-07-19 05:59:06 +0300

    Commit

---
+++ src/Maui.Controls.BottomSheet/BottomSheet.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace XGENO.Maui.Controls;
+
+public partial class BottomSheet : ContentView
+{
+    public IList<Microsoft.Maui.IView> BottomSheetContent => BottomSheetContentGrid.Children;
+
+    public BottomSheet()
+	{
+		InitializeComponent();
+	}
+
+	public async Task OpenBottomSheet()
+    {
+        await Task.CompletedTask;
+    }
+
+    public async Task CloseBottomSheet()
+    {
+        await Task.CompletedTask;
+    }
+}
--- src/Maui.Controls.BottomSheet/GlobalUsings.cs
+++ src/Maui.Controls.BottomSheet/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using global::System;
 global using global::Microsoft.Maui.Controls;
 global using global::Microsoft.Maui.Graphics;
+global using global::System.Collections.Generic;
+global using global::System.Threading.Tasks;
